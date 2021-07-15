690d6ada8a309e30f7413333831208d093fe5115
Mayank8085 <mayanksahu1806@gmail.com>
2021-07-15 17:44:00 +0530

    first Commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+ import Button from "./Button"
+import "./style.css"
+const  App=()=>{
+    return(
+        <div>
+        <h1 style={{textAlign: 'center'}}>Hello World !!</h1>
+        <Button title="Mayank"/>
+        <Button title="Sahu"/>
+        <Button/>
+
+
+        </div> 
+    ) 
+}
+
+export default App;
